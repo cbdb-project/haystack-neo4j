dd7680da2d3058117bd7bb4022f67edb26fc31fc
Hongsu Wang <sudospace@gmail.com>
2024-12-10 18:33:46 -0500

    update bge test

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-model_name = "Alibaba-NLP/gte-Qwen2-1.5B-instruct"
-dimention = 1536
+model_name = "BAAI/bge-large-zh-v1.5"
+dimention = 1024
 
 # candidates list
 # Conan-embedding-v1
+# BAAI/bge-large-zh-v1.5
+
+# Future testing
+# 1. BAAI/bge-m3
+# 2. Some suggestions for retrieval pipeline in RAG: https://huggingface.co/BAAI/bge-m3
--- haystck-inference.py
+++ haystck-inference.py
@@ -44,9 +44,9 @@ time_checkpoint = time.time()
 #)
 result = pipeline.run(
     data={
-        "text_embedder": {"text": "Chen Xiang"},
+        "text_embedder": {"text": "哪些人和王十朋有社會網絡關係？"},
         "retriever": {
-            "top_k": 5,
+            "top_k": 20,
         },
     }
 )
